5abdd3afee9a0cc47d11b3d0aa14ce86ace5739f
x54x6fx6d <tom31schule@web.de>
2022-09-03 23:31:16 +0200

    update and ip shit

--- client/website/src/App.js
+++ client/website/src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import config from "./config.json";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [ip,setIP] = useState("");
+  const getData = async()=>{
+    const res = await axios.get('https://checkip.amazonaws.com/');
+    console.log(res.data);
+    setIP(res.data.IPv4);
+  };
+  useEffect(()=>{
+    getData();
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,11 +21,15 @@ function App() {
         <a id="dev-site" class="text" href="https://tommy31.social/">
           Lotus Minecraft Server
         </a>
-        <p class="text">Test</p>
+        <input type="button" value="Test" onClick={() => {
+          window.location.assign(config.sendWhMessage + ip)
+        }}></input>
+        <p>{ip}</p>
         <input type="text" id="username" placeholder="Username"></input>
         <input type="button" id="btn" value="Start Server" onClick={()=>{
           let username = document.getElementById("username").value;
-          window.location.assign(`http://localhost:3000/api/post/minecraft/startServer/${username}`);
+          let cmd = config.startServerLink + username;
+          window.location.assign(cmd);
         }}></input>
       </header>
     </div>
